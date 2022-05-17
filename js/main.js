a5bf7735b3695eac1546dfaa9ef73802f08baf91
Treovan <94761962+Treovan@users.noreply.github.com>
2022-05-17 17:59:54 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function showHeader() {
-    let header = document.querySelector('.header');
-
-    if (window.scrollY > 400) {
-        header.classList.add('header_fixed');
-    }else {
-        header.classList.remove('header_fixed');
-    }
-};
